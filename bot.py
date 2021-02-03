8f41dd2f3c2b175cc398e47c252aec47f243b08e
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-03 10:07:59 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '-')
+
+@client.event
+async def on_ready():
+    print('Benet Bot is ready.')
+
+client.run('ODA2NTI3Mjc5Nzg1MzEyMjk3.YBqvEA.qz3c5BbkkMGjf4x2kKv8e2CsXSQ')
