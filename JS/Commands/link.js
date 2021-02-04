54241a620b1d9d66f06601cfda74e689f98e85a3
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-03 23:24:27 -0600

    Update link.js

--- JS/Commands/link.js
+++ JS/Commands/link.js
@@ -1,13 +1,16 @@
+// THIS GETS THE PERMANENT LIKE TO THE SERVER
+
 module.exports = {
-    name: 'link',
-    description: "Embed for link Command",
+    name: 'discord',
+    description: "Embed for discord Command",
     execute(message, args, Discord){
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#f3f3f3')
         .setTitle('**__Permanent Invite Link__**')
-        .setDescription(`https://discord.gg/aRKAMfsUH7`)
-        .setFooter('Benet Academy ©')
+// ENTER YOUR  SERVER LINK
+        .setDescription(`PUT YOUR SERVER'S LINK HERE`)
+        .setFooter('ENTER SERVER NAME HERE ©')
         .setTimestamp();
 
         message.channel.send(newEmbed);
