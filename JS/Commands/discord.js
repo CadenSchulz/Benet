b9add9747b9bc8f67686c1083c582e9041cd910e
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-03 19:56:18 -0600

    Add files via upload

---
+++ JS/Commands/discord.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'discord',
+    description: "Embed for discord Command",
+    execute(message, args, Discord){
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#f3f3f3')
+        .setTitle('**__Permanent Invite Link__**')
+        .setDescription(`https://discord.gg/aRKAMfsUH7`)
+        .setFooter('Benet Academy ©')
+        .setTimestamp();
+
+        message.channel.send(newEmbed);
+    }
+}
