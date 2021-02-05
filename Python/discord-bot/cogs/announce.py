5f56a9819834e418b5b9395485f2a64b9c4b6624
Narfee <nicholaspapciak@gmail.com>
2021-02-05 12:50:29 -0600

    fixed announce command

--- Python/discord-bot/cogs/announce.py
+++ Python/discord-bot/cogs/announce.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 
+
 class Announce(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='announce', aliases=["a"])
-    async def announce(self, ctx, *args):
-        for arg in args:
-            await ctx.send(arg)
+    async def announce(self, ctx, *, msg):
+        await ctx.send(msg)
+        await ctx.message.delete()
 
 
 def setup(bot):
